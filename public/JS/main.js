0832de5bff6f274630ea0a6a32bba2a0ef535d9d
Luis Eduardo Cruz Caballero <lecruz01@yahoo.com.mx>
2017-09-22 10:58:55 -0500

    Template Básico
    Se añade Bootstrap 4 para control de Grid. Se crea el template básico del visualizador. Se añade la API de Google Maps.

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,3 +1,7 @@
-$.ready(function() {
-    alert("Works!");
-});
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
--- public/index.php
+++ public/index.php
@@ -5,53 +5,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Visualizador de Viveros</title>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="CSS/main.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
     <header>
-        <nav>
-            <div class="navbar">
-                <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Administración</a></li>
-                    <li><a href="#">Visualización</a></li>
-                </ul>
-            </div>
-        </nav>
+      <nav class="row">
+        <h1 class="logo">Visualizador</h1>
+        <ul>
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Administración</a></li>
+          <li><a href="">Historicos</a></li>
+        </ul>
+      </nav>
     </header>
     
-    
-    <footer class="page-footer">
-    <div class="container">
+    <main class="container-fluid">
       <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">Footer Content</h5>
-          <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+        <div class="col-lg-8 col-sm-12">
+          <div id="map" class="col-sm-12"></div>
         </div>
-        <div class="col l4 offset-l2 s12">
-          <h5 class="white-text">Links</h5>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-          </ul>
+        <div class="col-lg-4 col-sm-12">
+          <div class="col-sm-12">
+            <h3>Herramientas</h3>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="footer-copyright">
+    </main>
+    
+    <footer>
       <div class="container">
-      © 2014 Copyright Text
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        <div class="row">
+          <div class="col-lg-6 col-sm-12">
+            <h5 class="white-text">Información del Sitio</h5>
+            <p class="grey-text text-lighten-4">
+              El proposito de este sitio es permitir visualizar los viveros localizados en México, así como permitir registrar la información historica de los mismos. 
+            </p>
+          </div>
+          <div class="col-lg-4 col-lg-offset-2 col-sm-12">
+            <h5 class="white-text">Enlaces de Interés</h5>
+            <ul>
+              <li>
+                <a class="grey-text text-lighten-3" href="https://developers.google.com/maps/documentation/javascript/tutorial?hl=es-419" target="_blank">
+                  Google Maps API
+                </a>
+              </li>
+              <li>
+                <a class="grey-text text-lighten-3" href="http://www.conafor.gob.mx/portal/" target="_blank">
+                  CONAFOR
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  </footer>
+      <div class="footer-copyright">
+        <div class="container">
+          copyright &copy 2017
+        </div>
+      </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="JS/bootstrap.min.js"></script>
     <script type="text/javascript" src="JS/main.js"></script>
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlFsOGuiesyTBdAWWygBZbRPZUH9e-Sog&callback=initMap" async defer></script>
 </body>
 </html>
